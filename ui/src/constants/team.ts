6bd58cf5a3eb7697f108e2108345fa24125a3adc
Ansh Jain <anshjain9159@gmail.com>
2025-05-03 10:49:38 +0530

    chore: frontend addition

---
+++ ui/src/constants/team.ts
@@ -0,0 +1,17 @@
+const teamMembers = [
+
+      
+      {
+        name: "Ansh Jain",
+        role: "Full Stack Developer",
+        image: "https://avatars.githubusercontent.com/AnshJain9159",
+        social: {
+          linkedin: "https://www.linkedin.com/in/ansh-jain-78986b242/",
+          github: "https://github.com/AnshJain9159",
+        }
+      },
+      
+      
+    ];
+  
+  export default teamMembers;
